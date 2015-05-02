c8fc2a7c1324b7cc7bcea6aea330b8b733206418
Kevin Hughes <kmh3785@gmail.com>
2015-05-02 10:06:47 -0600

    Initial commit

---
+++ dist/index.php
@@ -0,0 +1,1 @@
+<?php include('header.php'); ?><?php include('footer.php'); ?>
---
+++ src/footer.php
@@ -0,0 +1,3 @@
+<footer></footer>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script type="text/javascript" src="js/scripts.min.js"></script>
