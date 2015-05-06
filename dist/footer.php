da275aacd903b73bb062d9de2f16dee0155c1840
Kevin Hughes <kmh3785@gmail.com>
2015-05-06 15:18:15 -0600

    Remove dist

--- dist/footer.php
+++
@@ -1,1 +0,0 @@
-<footer></footer><script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script><script type="text/javascript" src="js/scripts.min.js"></script>
